eace8a169bc2508261da8680b681570728b98197
Zohaib khan <zk510873@gmail.com>
2025-12-29 18:50:31 -0800

    Product of array except self - Problem Implementation -Optimal Approach

---
+++ ProductOfArray-ExceptSelf-Problem-Optimal.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+	vector<int> nums; 
+	for(int i=1; i<5; i++) nums.push_back(i);
+	int n = nums.size();
+	vector<int> ans(n,1);
+	for(int i=1; i<n; i++){
+		ans[i] = ans[i-1]*nums[i-1];
+	}
+	int suffix = 1;
+	for(int i=n-2; i>=0; i--){
+		suffix *= nums[i+1];
+		ans[i] *= suffix;
+	}
+	cout<<"Array Product Except Self- Brute Force Approach";
+	cout<<"\n----------------------------------------------\n";
+	cout<<"Numbers: ";
+	for(int i=0; i<n; i++) cout<<nums[i]<<" ";
+	cout<<"\nProduct Except Self: ";
+	for(int i=0; i<n; i++) cout<<ans[i]<<" ";
+	return 0;
+}
