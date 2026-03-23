eb9b413372ed41ccd5fc4e90021428bcccb6f4b1
Zohaib khan <zk510873@gmail.com>
2026-03-23 04:26:05 -0700

    Stack implementation from scratch using link lists

---
+++ StackImplementationUsingLinkList.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<list>
+using namespace std;
+class Stack{
+	list<int> l;
+	public:
+		void push(int val){
+			l.push_front(val);
+		}
+		void pop(){
+			l.pop_front();
+		}
+		int top(){
+			return l.front();
+		}
+		bool empty(){
+			return l.size() == 0;
+		}
+};
+int main(){
+	cout<<"Stack Implementation using Lists...\n";
+	Stack s;
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	cout<<"Stack After pushing some elements: ";
+	while(!s.empty()){
+		cout<<s.top()<<"  ";
+		s.pop();
+	}
+	return 0;
+}
