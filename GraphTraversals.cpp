c9f577d43c859318ab10e8bf6ed277b4bf5a4ded
Zohaib khan <zk510873@gmail.com>
2026-03-24 23:39:53 -0700

    Breadth First search and Depth first search traversals algorithm implementations

---
+++ GraphTraversals.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<list>
+#include<queue>
+#include<vector>
+using namespace std;
+class Graph{
+	int V;
+	list<int> *l;
+	public:
+		Graph(int V){
+			this->V = V;
+			l = new list<int>[V];
+		}
+		void addEdge(int u, int v){
+			l[u].push_back(v);
+			l[v].push_back(u);
+		}
+		void printAdjList() {
+        	for (int i = 0; i < V; i++) {
+            	cout << i << " : ";
+           		for (int neighbor : l[i]) {
+                	cout << neighbor << "  ";
+            	}
+            	cout << endl;
+        	}
+    	}
+    	void bfs(){
+    		queue<int> q;
+    		vector<bool> vis(V,false);
+    		q.push(0);
+    		vis[0] = true;
+    		while(q.size() > 0){
+    			int u = q.front();
+    			q.pop();
+    			cout<<u<<"  ";
+    			for(int v:l[u]){
+    				if(!vis[v]){
+    					vis[v] = true;
+    					q.push(v);
+					}
+				}
+			}
+			cout<<endl;
+		}
+		void dfsHelper(int u, vector<bool> &vis){
+			cout<<u<<"  ";
+			vis[u] = true;
+			for(int v:l[u]){
+				if(!vis[v]){
+					dfsHelper(v,vis);
+				}
+			}
+		}
+		void dfs(){
+			int src = 0; 
+			vector<bool> vis(V,false);
+			dfsHelper(src,vis);
+		}
+};
+int main(){
+	cout<<"Graph Implementation...\n";
+	Graph g(5);
+	g.addEdge(0,1);
+	g.addEdge(1,2);
+	g.addEdge(1,3);
+	g.addEdge(2,4);
+	cout<<"Printing Adjacency Lists...\n";
+	g.printAdjList();
+	cout<<"Breadth First Search Traversal (BFS) Output...\n";
+	g.bfs();
+	cout<<"Depth First Search Traversal (DFS) Output...\n";
+	g.dfs();
+	return 0;
+}
